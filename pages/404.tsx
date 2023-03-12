c713c414b80ef28cffd148c70c6c34a32eed4023
hakakaku <libohaku@gmail.com>
2023-03-12 10:32:43 +0900

    feat: custom 404 page.

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+// pages/404.js
+export default function Custom404() {
+	return (
+		<section className="">
+			<h1 className="py-12 text-4xl leading-loose">404</h1>
+			<p> This page could not be found.</p>
+		</section>
+	);
+}
